63fb3a3b03903eba29c5d2b45f2b8e2d49d5333e
sidhuarm <61973427+sidhuarm@users.noreply.github.com>
2021-04-29 16:46:14 -0400

    This is ch1 and introduction to C

---
+++ input.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/*
+Learning user input in C programming
+Author: Arman Sidhu
+*/
+
+int main(){
+int a,b;
+
+    printf("Enter the value of a \n");
+    scanf("%d",&a);
+    printf("Enter the value of b \n");
+    scanf("%d",&b);
+    printf("The sum of both numbers are %d  \n",a+b);
+    return 0;
+
+}
