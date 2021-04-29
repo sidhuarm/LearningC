864453ec06cf4e1a8e9fa1fd797013b6380bddda
sidhuarm <61973427+sidhuarm@users.noreply.github.com>
2021-04-29 16:49:58 -0400

    Add files via upload

---
+++ CH2_Arithmatic/02_arithmetic_instruction.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main(){
+   int a = 5,b = 2; 
+   int k = 3.0/9;
+  printf("The sum of both a and b is %d \n",a+b);
+  printf("The multiplication of both a and b is %d \n",a*b);
+  printf("The division of both a and b is %f \n",5.0/b); 
+  printf("The sum of both a and b is %d \n",k);
+   return 0; 
+}
---
+++ CH2_Arithmatic/02_operatorprecidence.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main(){
+    int a;
+    printf("Enter the no you want to chech whether its divisble by 97 \n");
+    scanf("%d",&a);
+    int b = a%97;
+    printf("%d",b);
+    return 0;
+}
