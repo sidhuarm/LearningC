e44ba54c249e709776e8e8cd7275e2cb190cb035
sidhuarm <61973427+sidhuarm@users.noreply.github.com>
2021-05-04 12:25:02 -0400

    factorial algorithm with recursion

--- ch5_Functions_Recursions/ch_5_Functions_Recursion.c
+++ ch5_Functions_Recursions/ch_5_Functions_Recursion.c
@@ -3,3 +3,17 @@
 // Eg: Factorial with this algorithm
 // Author: Arman Sidhu
 #include <stdio.h>
+int factorial(int x);           // function prototype
+int main(){
+int a = 5;
+printf("The factorial of %d is %d",a,factorial(a));
+
+    return 0;
+}
+int factorial(int x){
+    if(x == 0 || x == 1){
+        return 1;
+    }else{
+        return x*factorial(x-1);
+    }
+}
